887c618e8dd7ff3c01a52a63bd53169117c7164f
Leonardo <leobarros_@outlook.com>
2026-04-03 21:13:53 -0300

    chore: initial setup

---
+++ src/main/java/com/leonardo/delivery_tracking_system/DeliveryTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.delivery_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/delivery_tracking_system/DeliveryTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.delivery_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
