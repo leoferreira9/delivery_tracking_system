6eff437fafec68335bb9912ffe0aa64ffadc2d06
Leonardo <leobarros_@outlook.com>
2026-04-06 21:49:44 -0300

    fix: fixing mapper issues, version handling and table naming

--- src/main/java/com/leonardo/delivery_tracking_system/mapper/DeliveryMapper.java
+++ src/main/java/com/leonardo/delivery_tracking_system/mapper/DeliveryMapper.java
@@ -1,22 +1,11 @@
 package com.leonardo.delivery_tracking_system.mapper;
 
-import com.leonardo.delivery_tracking_system.dto.delivery.DeliveryRequest;
 import com.leonardo.delivery_tracking_system.dto.delivery.DeliveryResponse;
 import com.leonardo.delivery_tracking_system.model.Delivery;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {AddressMapper.class, CustomerMapper.class, DelivererMapper.class, EstablishmentMapper.class})
+@Mapper(componentModel = "spring")
 public interface DeliveryMapper {
 
-    @Mapping(source = "customer.name", target = "customerName")
-    @Mapping(source = "customer.phone", target = "customerPhone")
-    @Mapping(source = "deliverer.name", target = "delivererName")
-    @Mapping(source = "establishment.name", target = "establishmentName")
     DeliveryResponse toDto(Delivery delivery);
-
-
-    @Mapping(source = "customerId", target = "customer.id")
-    @Mapping(source = "establishmentId", target = "establishment.id")
-    Delivery toEntity(DeliveryRequest request);
 }
--- src/main/java/com/leonardo/delivery_tracking_system/model/Customer.java
+++ src/main/java/com/leonardo/delivery_tracking_system/model/Customer.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "customers")
 public class Customer {
 
     @Id
