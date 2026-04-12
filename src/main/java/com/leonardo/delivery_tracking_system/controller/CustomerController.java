014fbec8dacb324fdad0151cd148b58068a018b5
Leonardo <leobarros_@outlook.com>
2026-04-12 00:18:41 -0300

    feat: version API endpoints to /v1

--- src/main/java/com/leonardo/delivery_tracking_system/controller/CustomerController.java
+++ src/main/java/com/leonardo/delivery_tracking_system/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers/v1")
+@RequestMapping("/v1/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
