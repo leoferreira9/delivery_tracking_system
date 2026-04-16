87d88c009bd262d5a6ea9f04adb99ef9a7d92cdb
Leonardo <leobarros_@outlook.com>
2026-04-15 23:48:13 -0300

    fix: fix delete response in Deliverer controller

--- src/main/java/com/leonardo/delivery_tracking_system/controller/DelivererController.java
+++ src/main/java/com/leonardo/delivery_tracking_system/controller/DelivererController.java
@@ -42,7 +42,7 @@ public class DelivererController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<DelivererResponse> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id){
         delivererService.delete(id);
         return ResponseEntity.noContent().build();
     }
