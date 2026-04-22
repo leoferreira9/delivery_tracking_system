0fcea47e647c424dfc56c791b1250f0b10a603c2
Leonardo <leobarros_@outlook.com>
2026-04-22 19:48:53 -0300

    feat: add regex validation for CNPJ and CPF in request DTOs

--- src/main/java/com/leonardo/delivery_tracking_system/dto/customer/CustomerRequest.java
+++ src/main/java/com/leonardo/delivery_tracking_system/dto/customer/CustomerRequest.java
@@ -1,14 +1,11 @@
 package com.leonardo.delivery_tracking_system.dto.customer;
 
 import com.leonardo.delivery_tracking_system.dto.address.AddressRequest;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CustomerRequest(
         @NotBlank @Size(max = 255) String name,
-        @NotBlank @Size(max = 16) String cpf,
+        @NotBlank @Size(max = 16) @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$") String cpf,
         @NotBlank @Size(max = 20) String phone,
         @NotBlank @Email String email,
         @NotNull AddressRequest address
--- src/main/java/com/leonardo/delivery_tracking_system/dto/establishment/EstablishmentRequest.java
+++ src/main/java/com/leonardo/delivery_tracking_system/dto/establishment/EstablishmentRequest.java
@@ -3,11 +3,12 @@ package com.leonardo.delivery_tracking_system.dto.establishment;
 import com.leonardo.delivery_tracking_system.dto.address.AddressRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record EstablishmentRequest(
         @NotBlank @Size(max = 255) String name,
-        @NotBlank @Size(max = 255) String cnpj,
+        @NotBlank @Size(max = 255) @Pattern(regexp = "^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$") String cnpj,
         @NotBlank @Size(max = 20) String phone,
         @NotNull AddressRequest address
 ) {}
